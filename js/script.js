9e19d476d99f60ce624bdab9038ed362f889ed8c
Abhinav Kumar <abhinavvashishthaspj@gmail.com>
2024-02-09 15:04:51 +0530

    Till Latest Notification

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Stop Marquee 
+const marquee = document.getElementById('marqueeStop');
+
+marquee.addEventListener('mouseover', function () {
+    this.stop();
+});
+
+marquee.addEventListener('mouseout', function () {
+    this.start();
+});
+// Stop Marquee 
