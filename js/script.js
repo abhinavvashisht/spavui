cee7c8cb06de3665bebdba06fedbbaec2b1a233d
Abhinav Kumar <abhinavvashishthaspj@gmail.com>
2024-02-09 16:07:36 +0530

    Till Tost

--- js/script.js
+++ js/script.js
@@ -9,3 +9,18 @@ marquee.addEventListener('mouseout', function () {
     this.start();
 });
 // Stop Marquee 
+
+
+// Script to show the toast on page load 
+document.addEventListener('DOMContentLoaded', function () {
+    var toastTrigger = document.getElementById('liveToastBtn');
+    var toast = new bootstrap.Toast(document.getElementById('liveToast'));
+
+    toastTrigger.addEventListener('click', function () {
+        toast.show();
+    });
+
+    // Trigger toast on page load
+    toast.show();
+});
+// Script to show the toast on page load  
