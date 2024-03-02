5db69829107b3bf012dcb9ae885d898814f7941f
Abhinav Kumar <abhinavvashishthaspj@gmail.com>
2024-03-02 08:17:31 +0530

    Little addon in index page

--- js/script.js
+++ js/script.js
@@ -11,16 +11,3 @@ marquee.addEventListener('mouseout', function () {
 // Stop Marquee 
 
 
-// Script to show the toast on page load 
-document.addEventListener('DOMContentLoaded', function () {
-    var toastTrigger = document.getElementById('liveToastBtn');
-    var toast = new bootstrap.Toast(document.getElementById('liveToast'));
-
-    toastTrigger.addEventListener('click', function () {
-        toast.show();
-    });
-
-    // Trigger toast on page load
-    toast.show();
-});
-// Script to show the toast on page load  
